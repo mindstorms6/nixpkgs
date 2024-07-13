c83e5f261d0ac50ead44d911280b5af91411360e
Breland Miley <breland@bdawg.org>
2024-07-13 16:44:42 -0400

    Add music-assisant custom component

--- pkgs/servers/home-assistant/custom-components/music-assistant/default.nix
+++ pkgs/servers/home-assistant/custom-components/music-assistant/default.nix
@@ -1,33 +1,31 @@
-{ lib
-, fetchFromGitHub
-, buildHomeAssistantComponent
-, music-assistant
+{
+  lib,
+  fetchFromGitHub,
+  buildHomeAssistantComponent,
+  music-assistant,
 }:
 
 buildHomeAssistantComponent rec {
   owner = "music-assistant";
-  domain = "hass_music_assitant";
+  domain = "mass";
   version = "2024.6.2";
 
   src = fetchFromGitHub {
     owner = "music-assistant";
     repo = "hass-music-assistant";
     rev = "refs/tags/${version}";
-    hash = "";
+    hash = "sha256-Wvc+vUYkUJmS4U34Sh/sDCVXmQA0AtEqIT8MNXd++3M=";
   };
 
-  propagatedBuildInputs = [
-    music-assistant
-  ];
+  dependencies = [ music-assistant ];
 
-  #skip phases with nothing to do
-  # dontConfigure = true;
-  # dontBuild = true;
-  # doCheck = false;
+  postPatch = ''
+    substituteInPlace custom_components/mass/manifest.json --replace-fail 'music-assistant==2.0.6' 'music-assistant'
+  '';
 
   meta = with lib; {
     changelog = "https://github.com/music-assistant/hass-music-assistant/releases/tag/${version}";
-    description = "Custom integration for Moonraker and Klipper in Home Assistant";
+    description = "Custom integration for Music Assistant in Home Assistant";
     homepage = "https://github.com/music-assistant/hass-music-assistant";
     maintainers = with maintainers; [ mindstorms6 ];
     license = licenses.asl20;
